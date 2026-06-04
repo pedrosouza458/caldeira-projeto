d17012a78fb9254408564134802539cd85ee3ab9
Vitor Saucedo Uggeri <vitorsaucedo18@outlook.com>
2026-06-04 15:59:01 -0300

    chore: initial spring boot project setup with modular architecture

---
+++ backend/src/main/java/com/gc/projeto/company/Company.java
@@ -0,0 +1,53 @@
+package com.gc.projeto.company;
+
+import com.gc.projeto.company.enums.CompanyType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "companies")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Company {
+
+    @Id
+    @UuidGenerator
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "logo", nullable = false)
+    private String logo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
+    private CompanyType type;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = Instant.now();
+        updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
+}
+
+
